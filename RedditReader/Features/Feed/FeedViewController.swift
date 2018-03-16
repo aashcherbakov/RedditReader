230f558fc900e4ab605ed68161c29855c0215d5c
Alex Shcherbakov <alexander@prolificinteractive.com>
2018-03-15 21:27:11 -0700

    Setup FeedViewController

---
+++ RedditReader/Features/Feed/FeedViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FeedViewController.swift
+//  RedditReader
+//
+//  Created by Alex Shcherbakov on 3/15/18.
+//  Copyright © 2018 Oleks Shcherbakov. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class FeedViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
+}
